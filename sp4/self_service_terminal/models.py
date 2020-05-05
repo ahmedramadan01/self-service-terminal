9943727ad350314353e4daa1a7cd3e241eb339c7
Ahmed-Ramadan <ahmed.ramadan@tu-ilmenau.de>
2020-05-05 15:00:46 +0200

    add __str__method in Form model

--- sp4/self_service_terminal/models.py
+++ sp4/self_service_terminal/models.py
@@ -6,6 +6,9 @@ class Form(models.Model):
     show_on_frontend = models.BooleanField()
     description = models.CharField(max_length=120)
 
+    def __str__(self):
+        return self.description
+
 class Which_year(models.Model):
     form = models.ForeignKey(Form,on_delete = models.CASCADE)
     which_year_text = models.CharField(max_length=4)
