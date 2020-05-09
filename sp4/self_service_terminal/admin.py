ae48d7acdc69e2570b5b55bcf72728fa74bfeb19
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-05-09 13:31:24 +0200

    Added FormAdmin
    Added fieldsets with Descriptions of fields.

--- sp4/self_service_terminal/admin.py
+++ sp4/self_service_terminal/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Form, Menu
 
-admin.site.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Übergeordnetes Menü', {'fields': ['parent_menu']}),
+        ('Anzeigen',            {'fields': ['show_on_frontend']}),
+        ('Details',             {'fields': ['form_title', 'description']})
+        ]
+        
+admin.site.register(Form, FormAdmin)
+
 admin.site.register(Menu)
