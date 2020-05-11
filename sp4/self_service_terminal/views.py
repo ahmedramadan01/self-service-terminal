ec21ba5d71bb1e453bee8f84af3075f29fcc4862
Ahmed-Ramadan <ahmed.ramadan@tu-ilmenau.de>
2020-05-11 23:55:46 +0200

    added formular.html its url and view

--- sp4/self_service_terminal/views.py
+++ sp4/self_service_terminal/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 def index(request):
     return render(request, 'self_service_terminal/index.html')
+
+def formular(request):
+    return render(request, 'self_service_terminal/formular.html')
