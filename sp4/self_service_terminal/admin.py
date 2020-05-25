87119d97312d4e89c63d15847a75a57ffcea8570
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-05-25 11:36:18 +0200

    Add admin fields for the Design model

--- sp4/self_service_terminal/admin.py
+++ sp4/self_service_terminal/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Form, Menu, Startpage
+from .models import Form, Menu, Startpage, Design
+
+class DesignAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Farbe',           {'fields': ['colorval']}),
+        ('Logo',            {'fields': ['institute_logo']})
+    ]
 
 class StartpageAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -25,6 +31,7 @@ class FormAdmin(admin.ModelAdmin):
     list_display = ['form_title', 'time_since_last_updated_str', 'upload_date', 'last_changed']
     list_filter = ['upload_date','last_changed']
 
+admin.site.register(Design, DesignAdmin)
 admin.site.register(Startpage, StartpageAdmin)
 admin.site.register(Menu, MenuAdmin)
 admin.site.register(Form, FormAdmin)
