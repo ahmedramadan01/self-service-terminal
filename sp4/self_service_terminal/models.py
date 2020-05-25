f9fc91e32446f75106abef7dcebfe8496702051f
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-05-25 14:04:38 +0200

    Change startpage field to OneToOneField

--- sp4/self_service_terminal/models.py
+++ sp4/self_service_terminal/models.py
@@ -16,7 +16,7 @@ class Startpage(models.Model):
 
 class Menu(models.Model):
     """Model the menus and submenus of the self service terminal"""
-    startpage = models.ForeignKey(Startpage, on_delete=models.CASCADE, blank=True, null=True)
+    startpage = models.OneToOneField(Startpage, on_delete=models.CASCADE)
     parent_menu = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
     menu_title = models.CharField(max_length=100)
     menu_text = models.TextField(blank=True)
