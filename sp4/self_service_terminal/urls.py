405f765d055e4454c452d088bb42d29514ba4d60
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-05-28 15:31:02 +0200

    Add name argument to urlpatterns

--- sp4/self_service_terminal/urls.py
+++ sp4/self_service_terminal/urls.py
@@ -7,8 +7,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('menu/<int:menu_id>/', views.menu),
-    path('menu/<str:menu_title>/', views.menu),
-    path('form/<int:form_id>/', views.formular),
-    path('form/<str:form_title>/', views.formular)
+    path('menu/<int:menu_id>/', views.menu, name="menu_id"),
+    path('menu/<str:menu_title>/', views.menu, name="menu_title"),
+    path('form/<int:form_id>/', views.formular, name="form_id"),
+    path('form/<str:form_title>/', views.formular, name="form_title")
 ]
