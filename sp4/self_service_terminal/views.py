826bc58af8f0b59d8a60b2a391b429ed90523ab6
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-05-30 20:04:10 +0200

    Add context parameters for menu and form view

--- sp4/self_service_terminal/views.py
+++ sp4/self_service_terminal/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from .models import Form
 
 def index(request):
     """Return the bare base.html."""
@@ -8,17 +9,52 @@ def index(request):
 in the database then return the homepage."""
 
 def menu(request, menu_id=None, menu_title=None):
-    """TODO Return the menu with the primary key <menu_id>."""
-
-    return render(request, 'self_service_terminal/menu.html') 
+    """TODO Return the menu with the primary key <menu_id>.
+    
+    Parameters:
+    - id_homepage
+    - id_parent_menu
+    - submenus : {
+        menu1_id : {
+            menu1_name : 'somename',
+            menu1_id   : 101010
+        },
+        ...
+    }
+    - number_of_submenus
+    """
+    context = {
+        'id_homepage'       : 12345,
+        'id_parent_menu'    : 54321,
+        'submenus'          : {
+            10101 : {
+                'name'  : 'Ich bin ein Menü.',
+                'id'    : 10101
+            }
+        }
+    }
+    context['number_of_submenus'] = len(context['submenus'])
+    return render(request, 'self_service_terminal/menu.html', context)
 
 def formular(request, form_id=None, form_title=None):
-    """TODO Return the form with the primary key <form_id>."""
+    """TODO Return the form with the primary key <form_id>.
+    
+    Parameters:
+    - id_homepage
+    - id_parent
+    - number_of_copies
+    """
+    context = {
+        'id_homepage'       : 1234,
+        'id_parent'         : 54321,
+        'number_of_copies'  : 100000000
+    }
     return render(request, 'self_service_terminal/formular.html')
 
 def print_formular(request, form_id=None):
     """TODO Just run the print function of the given form 
     and do not change the current page"""
+    # Form.objects.get('id=').print_form()
     return render(request, 'self_service_terminal/formular.html')
 
 # Testview für die Django Templatesprache
