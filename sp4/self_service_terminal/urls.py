e2fa879c8eecface003883783977258a88795d25
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-06-01 13:10:10 +0200

    Print Button
    Make Print Button do something,
    Add current id of form to context variables,
    template adjusted accordingly

--- sp4/self_service_terminal/urls.py
+++ sp4/self_service_terminal/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('menu/<int:menu_id>/', views.menu, name="menu_id"),
     path('menu/<str:menu_title>/', views.menu, name="menu_title"),
     path('form/<int:form_id>/', views.formular, name="form_id"),
+    path('form/<int:form_id>/print', views.print_formular, name="form_print"),
     path('form/<str:form_title>/', views.formular, name="form_title"),
     
     # Django Templatesprache Test
