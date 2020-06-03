19fbacf80b4f65bf7c85ff5386be015db9b923c5
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-06-03 13:55:08 +0200

    Confrom PEP8

--- sp4/self_service_terminal/admin.py
+++ sp4/self_service_terminal/admin.py
@@ -4,30 +4,31 @@ from .models import Form, Menu, Terminal_Settings
 
 from import_export.admin import ImportExportModelAdmin
 
+
 class MenuInline(admin.TabularInline):
     model = Menu
     extra = 0
+
 
 class FormInline(admin.TabularInline):
     model = Form
     extra = 0
 
 
-
 @admin.register(Terminal_Settings)
 class Terminal_SettingsAdmin(ImportExportModelAdmin):
     fieldsets = [
-        ( 'Beschreibung', {'fields': ['title', 'description']}),
-        ('Farbe', 
+        ('Beschreibung', {'fields': ['title', 'description']}),
+        ('Farbe',
             {'fields': [
-                    'colorval_nav_bar', 
-                    'colorval_heading', 
-                    'colorval_text', 
-                    'colorval_button', 
-                    'colorval_zurueck_button']
-            }),
+                'colorval_nav_bar',
+                'colorval_heading',
+                'colorval_text',
+                'colorval_button',
+                'colorval_zurueck_button']
+             }),
         ('Logos', {'fields': ['institute_logo', 'krankenkasse_logo']})
-        ]
+    ]
 
 
 @admin.register(Menu)
@@ -39,15 +40,17 @@ class MenuAdmin(ImportExportModelAdmin):
     ]
     inlines = [MenuInline, FormInline]
 
+
 class FormAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Übergeordnetes Menü', {'fields': ['parent_menu']}),
         ('Anzeigen',            {'fields': ['show_on_frontend']}),
         ('Details',             {'fields': ['form_title', 'description']}),
         ('Dateipfad',           {'fields': ['pdffile']})
-        ]
-    list_display = ['form_title', 'time_since_last_updated_str', 'upload_date', 'last_changed']
-    list_filter = ['upload_date','last_changed']
+    ]
+    list_display = ['form_title', 'time_since_last_updated_str',
+                    'upload_date', 'last_changed']
+    list_filter = ['upload_date', 'last_changed']
 
 # admin.site.register(Terminal_Settings, Terminal_SettingsAdmin)
 # admin.site.register(Menu, MenuAdmin)
