e8d329243061945156cae0e739c4f3babfe49906
Linziqizoe <Linziqizoe@Linziqizoe-3.local>
2020-06-04 10:18:21 +0200

    drunken

--- sp4/sp4/print_form.py
+++ sp4/sp4/print_form.py
@@ -1,26 +1,38 @@
-import os
+import subprocess
 def isRun():
-    if os.system('lpstat -r'):
-        return True
-        else:
-            return False
-def gerPrinter():
-    printer = os.system('lpstat -a')
-    if printer is None:
-        return None
-        else:
-            return printer
-def runPrinter(printerName,fileName):
-    os.system('lpr -P -#'+''+num+''+''+printerName+''+fileName)
+	if cmd('lpstat -r'):
+		return True
+	else:
+		return False
+
+def getPrinter():
+	printer = cmd('lpstat -a')
+	if printer is None:
+		return None
+	else:
+		return printer
+
+def runPrinter(printerName,num,fileName):
+	printstring = 'lpr -P {p} -# {n} {file}'
+	cmd(printstring.format(p=printerName,n=num,file=fileName))
 
 def start():
-    if isRun():
-        printerName = getPrinter()
-        if not (printer Name is Nome):
-            runPrinter(printerName,1,"pdfname")
-            else:
-                print('Kein frei Druken')
+	if isRun():
+		printerName = getPrinter()
+		if not (printerName is None):
+			runPrinter(printerName,1,"pdfname")
+		else:
+			print('Keinen freien Druker')
+	else:
+		print('No CUPS')
+		
+def cmd(command):
+    subp = subprocess.Popen(command,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding="utf-8")
+    subp.wait(2)
+    if subp.poll() == 0:
+        return subp.communicate()[1]
     else:
-        print('No CUPS')
+        return None
 
-start()
+if __name__ == '__main__' :
+	start()
