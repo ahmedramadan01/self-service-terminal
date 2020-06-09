60732c471324ece3e4e1fa04ba44898516fbe136
Ahmed-Ramadan <ahmed.ramadan@tu-ilmenau.de>
2020-06-09 18:59:48 +0200

    terminal_settings_ class beschreibung

--- doxy_code_documentation/html/search/functions_0.js
+++ doxy_code_documentation/html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5f_5fstr_5f_5f_26',['__str__',['../classself__service__terminal_1_1models_1_1_terminal___settings.html#ac2d14a182dc6e232f1485896c74167d5',1,'self_service_terminal::models::Terminal_Settings']]]
+  ['_5f_5fstr_5f_5f_26',['__str__',['../classself__service__terminal_1_1models_1_1_terminal___settings.html#ac2d14a182dc6e232f1485896c74167d5',1,'self_service_terminal.models.Terminal_Settings.__str__()'],['../classself__service__terminal_1_1models_1_1_menu.html#a5b4e27681661efe9d3806bfc991d5d6e',1,'self_service_terminal.models.Menu.__str__()']]]
 ];
