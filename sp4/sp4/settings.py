39e03090cfe0ee3c76d94eab8904a3719159d7c5
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-06-16 19:29:04 +0200

    Undo static root stuff

--- sp4/sp4/settings.py
+++ sp4/sp4/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(Path.home(), '.self_service_terminal/static')
+STATIC_ROOT = os.path.join(BASE_DIR, '.self_service_terminal/static')
 
 STATIC_URL = '/static/'
 
