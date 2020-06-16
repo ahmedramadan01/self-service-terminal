d32aab3fbc2cdd8ea48d1a72e7b9aa1d6de41a43
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-06-16 18:38:57 +0200

    Adjust URL scheme

--- sp4/self_service_terminal/urls.py
+++ sp4/self_service_terminal/urls.py
@@ -7,14 +7,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# TODO Customize error view to automatically return to homepage
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu/<int:menu_id>/', views.menu, name="menu_id"),
     path('menu/<str:menu_title>/', views.menu, name="menu_title"),
     path('form/<int:form_id>/', views.formular, name="form_id"),
     path('form/<int:form_id>/print', views.print_formular, name="form_print"),
+    path('form/<int:form_id>/print/', views.print_formular, name="form_print"),
     path('form/<str:form_title>/', views.formular, name="form_title"),
 
     # Django Templatesprache Test
