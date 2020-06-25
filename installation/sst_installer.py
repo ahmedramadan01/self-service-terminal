bf5aa27259e0aa0e51aee935e2efdbd1a611139c
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-06-25 11:59:07 +0200

    Use subprocess module

--- installation/sst_installer.py
+++ installation/sst_installer.py
@@ -3,25 +3,26 @@ It is recommended to call the script from within a Python virtual environment.
 """
 
 import os
+from subprocess import run
 import getpass
 
 # Prepare Raspberry Pi OS:
-os.system("sudo apt update") 
-os.system("sudo apt upgrade")
-os.system("sudo apt install python3 apache2 apache2-dev cups -y")
+run("sudo apt update", shell=True) 
+run("sudo apt upgrade", shell=True)
+run("sudo apt install python3 apache2 apache2-dev cups -y", shell=True)
 
 # Python:
-os.system("pip3 install django mod_wsgi django-import-export Pillow pdf2image")
+run("pip3 install django mod_wsgi django-import-export Pillow pdf2image", shell=True)
 
 # Wlan, currently not operational
 
-os.system("sudo apt install hostpad")
+run("sudo apt install hostpad", shell=True)
 
-os.system("sudo systemctl unmask hostapd")
-os.system("sudo systemctl enable hostapd")
-os.system("sudo apt install dnsmasq")
+run("sudo systemctl unmask hostapd", shell=True)
+run("sudo systemctl enable hostapd", shell=True)
+run("sudo apt install dnsmasq", shell=True)
 
-os.system("sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent")
+run("sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent", shell=True)
 
 # config StaticIP, edit config file for dhcp
 
@@ -35,17 +36,17 @@ with open("/etc/sysctl.d/routed-ap.conf", mode = "w") as fp:
 
 # adding firewall rules
 
-os.system("sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE")
-os.system("sudo netfilter-persistent save")
+run("sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE", shell=True)
+run("sudo netfilter-persistent save", shell=True)
 
 # configure dhcp and dns for wlan, NICHT FUNKTIONAL
 # rename default config file and edit new one
  
-os.system("sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.old")
+run("sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.old", shell=True)
 with open("/etc/dnsmasq.conf", mode="w") as fp:
     fp.write("""interface=wlan0\t# ListeningInterface\ndhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h\t# Pool of IP addresses served via DHCP\ndomain=wlan\t# Local wireless DNS domain\naddress=/gw.wlan/192.168.4.1\t# Alias for this router""")
 # ensure wireless operation
-os.system("sudo rfkill unblock wlan")
+run("sudo rfkill unblock wlan", shell=True)
 
 # configure access point software
 # create config file
@@ -74,7 +75,7 @@ with open("/etc/hostapd/hostapd.conf", mode='w') as fp:
 # FILL IN INFO FROM DOCUMENTATION
 check = input("Um die Einrichtung abzuschließen, muss das System neu gestartet werden. Wollen Sie das jetzt tun? (y/n)")
 if check == "y":
-    os.system("sudo systemctl reboot")
+    run("sudo systemctl reboot", shell=True)
 
 
 
