19f2fc3316578c37a399a947bb444ba274c07e4d
Hakeem Shaddoud <hakeem.shaddoud@tu-ilmenau.de>
2020-06-27 02:32:59 +0200

    welcome screen after 2 min inactivity

--- sp4/self_service_terminal/static/self_service_terminal/app.js
+++ sp4/self_service_terminal/static/self_service_terminal/app.js
@@ -1,18 +1,47 @@
+var timeoutInMiliseconds = 120000;
+var timerId;
 var el = document.querySelector("#homepage");
 var el2 = document.querySelector("#start");
 var el3 = document.querySelector("#welcome");
 
 if (el2 != null) {
     el2.addEventListener("click", function () {
-
         el.style.display = '';
         el3.parentNode.removeChild(el3);
-
-
-
     });
 }
 
+function startTimer() {
+    timerId = window.setTimeout(goHome, timeoutInMiliseconds)
+}
+function resetTimer() {
+    window.clearTimeout(timerId)
+    startTimer();
+}
+function goHome() {
+    window.location.replace("/");
+}
+function setupTimers() {
+    document.addEventListener("mousemove", resetTimer, false);
+    document.addEventListener("mousedown", resetTimer, false);
+    document.addEventListener("keypress", resetTimer, false);
+    document.addEventListener("touchmove", resetTimer, false);
+    startTimer();
+}
+function ready(callbackFunc) {
+    if (document.readyState !== 'loading') {
+        callbackFunc();
+    } else if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', callbackFunc);
+    } else {
+        document.attachEvent('onreadystatechange', function () {
+            if (document.readyState === 'complete') {
+                callbackFunc();
+            }
+        });
+    }
+}
 
-
-
+ready(function () {
+    setupTimers();
+});
