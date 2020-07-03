1c47a4ddc95d1cc7338256864f86458c300fcc8b
David Hruschka <david-bruno.hruschka@tu-ilmenau.de>
2020-07-03 13:53:31 +0200

    Add another host

--- sp4/sp4/settings.py
+++ sp4/sp4/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'hvyf2=t2mhmrpo0)tlecy#uv#(ge9n*-l33^17@na*ap49-#g)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.4.1']
+ALLOWED_HOSTS = ['192.168.4.1', '192.168.2.200']
 
 
 # Application definition
